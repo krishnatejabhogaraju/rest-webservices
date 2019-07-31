cd4740e11fd09f0f0624a81a5c9dcc7f69c27b50
krishna <bhogarajukrishnateja@gmail.com>
2019-07-31 10:20:08 +0530

    Error

--- rest-ful-webservices/src/main/java/com/spring/rest/webservices/bean/HelloWorldBean.java
+++ rest-ful-webservices/src/main/java/com/spring/rest/webservices/bean/HelloWorldBean.java
@@ -2,7 +2,7 @@ package com.spring.rest.webservices.bean;
 
 public class HelloWorldBean {
 
-	private String message;
+	private String message1;
 
 	public HelloWorldBean(String message) {
 		this.message = message;
