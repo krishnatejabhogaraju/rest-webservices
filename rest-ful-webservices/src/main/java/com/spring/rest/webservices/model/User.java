4581d65909c5908b7659d63fe17a28c12b28d47a
Krishna teja <bhogarajukrishnateja@gmail.com>
2019-08-06 14:43:21 +0530

    Swagger implementation

--- rest-ful-webservices/src/main/java/com/spring/rest/webservices/model/User.java
+++ rest-ful-webservices/src/main/java/com/spring/rest/webservices/model/User.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "User Details")
 public class User {
 
 	private int id;
 	
 	@Size(min = 2,message = "Name should atleast have two characters")
+	@ApiModelProperty(notes = "Name should Have Atleast Two Characters")
 	private String name;
 	
 	@Past
+	@ApiModelProperty(notes = "Only past Date")
 	private Date dob;
 
 	public int getId() {
